cec76ddb0c7395e70ec6750a96a6713de5b9f46b
zainbsuliman <zat301099@gmail.com>
2024-10-19 16:03:11 +0300

    Initial Commit

---
+++ LearningProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningProject
+//
+//  Created by Zainab Alatwi  on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningProject/LearningProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningProjectApp.swift
+//  LearningProject
+//
+//  Created by Zainab Alatwi  on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
