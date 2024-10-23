9989bca96bb3711b8321d5cd87da6f793381922c
zainbsuliman <zat301099@gmail.com>
2024-10-23 09:22:54 +0300

    ADD

---
+++ LearningProject/SecoundPage.swift
@@ -0,0 +1,18 @@
+//
+//  SecoundPage.swift
+//  LearningProject
+//
+//  Created by Zainab Alatwi  on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+struct SecoundPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecoundPage()
+}
---
+++ LearningProject/ex.swift
@@ -0,0 +1,168 @@
+//
+//  FirstPage.swift
+//  LearningProject
+//
+//  Created by Zainab Alatwi  on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct FirstPage2: View {
+    @State var backgraundColor: Color = Color.black
+    
+    @State  var buttenTextColor1 = false
+    @State  var buttenTextColor2 = false
+    @State  var buttenTextColor3 = false
+    
+   // @State private var buttenColor: Color = Color.gray.opacity(0.5)
+    @State var userinput: String = ""
+    var body: some View {
+        ZStack {
+            
+            backgraundColor
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+            
+            VStack(spacing:20){
+                
+                Text("🔥")
+                    .font(.system(size: 50))
+                    .background(
+                        Circle()
+                            .fill(Color.gray.opacity(0.6))
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .black, radius: 3)
+                            .opacity(0.4)
+                    )
+                    .offset(y:-15)
+                
+                Text("Hello Learner!")
+                    .font(.largeTitle)
+                    .padding(.leading, -140)
+                    .bold()
+                
+                Text("This app will help you learn everyday")
+                    .padding(.leading, -90)
+                    .opacity(0.5)
+                
+                Text("I want to learn")
+                    .padding(.leading, -180)
+                    .bold()
+                
+                VStack {
+                    TextField("Swift", text: $userinput)
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color.gray.opacity(0.5))
+                    
+                    Text("I want to learn it in a")
+                        .padding(.leading, -180)
+                        .offset(y:15)
+                        .bold()
+                }
+                
+                .tint(.orange)
+                
+                
+            }.foregroundColor(.white)
+                .offset(y: -190)
+            
+            
+            HStack{
+                
+                Button(action: {
+                    buttenTextColor1.toggle()
+                    
+                }){
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 40)
+                        .foregroundColor((buttenTextColor1 ? .orangeC : .gray.opacity(0.4)))
+                    
+                    //.foregroundColor(buttenColor)
+                        .overlay(
+                            Text("Week")
+                                .foregroundColor(buttenTextColor1 ?.black : .orangeC)
+                            //.background(buttenTextColor1 ?.orangeC : .black)
+                        )
+                    
+                    
+                }
+                
+                
+                
+                Button(action: {
+                    
+                    buttenTextColor2.toggle()
+                }){
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 40)
+                        .foregroundColor((buttenTextColor2 ? .orangeC : .gray.opacity(0.4)))
+                    
+                    //.foregroundColor(buttenColor)
+                        .overlay(
+                            Text("Month")
+                            
+                                .foregroundColor(buttenTextColor2 ?.black :.orangeC)
+                        )
+                    
+                    
+                }
+                
+                Button(action: {
+                    
+                    buttenTextColor3.toggle()
+                    
+                }){
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 100, height: 40)
+                        .foregroundColor((buttenTextColor3 ? .orangeC : .gray.opacity(0.4)))
+                    
+                    //  .foregroundColor(buttenColor)
+                        .overlay(
+                            Text("Year")
+                                .foregroundColor(buttenTextColor3 ?.black : .orangeC)
+                        )
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }.offset(y:20)
+            
+            Button(action: {
+                
+                //
+            }){
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 151, height: 52)
+                    .foregroundColor(.orangeC)
+                
+                    .overlay(){
+                        Image(systemName: "arrow.forward")
+                            .foregroundColor(.black)
+                            .offset(x:30)
+                            .bold()
+
+                        Text("Start")
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                
+            }.offset(y:130)
+        
+    
+            
+        }
+        
+    }
+    
+    
+}
+
+
+#Preview {
+    FirstPage2()
+}
